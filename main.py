1515f2ebfe3252417c5579d1b7b6a57f37a84e37
xaviac <jacevedomx@gmail.com>
2024-05-02 18:48:25 -0600

    Refactorizar funciones de recomendación de juegos por año

--- main.py
+++ main.py
@@ -63,22 +63,18 @@ async def UserForGenre(genero: str):
 async def UsersRecommend(year: int):
     """Función que devuelve los 3 juegos más recomendados para un año dado."""
     # Filtramos el DataFrame donde la columna 'release_year' es igual a year, la columna 'recommend' es True y la columna 'sentiment_analysis' tiene valores 1 o 2. 
-    filter = union_ur_sg['title'][(union_ur_sg['release_year'] == year) & (union_ur_sg['recommend'] is True) & (union_ur_sg['sentiment_analysis'].isin([1, 2]))].value_counts().reset_index().head(3)
-
-    result = [{'Puesto {}: {}'.format(i + 1, row['title'])} for i, row in filter.iterrows()]
+    filter = union_ur_sg['title'][(union_ur_sg['release_year'] == year) & (union_ur_sg['recommend'] == True) & (union_ur_sg['sentiment_analysis'].isin([1, 2]))].value_counts().reset_index().head(3)
     
-    return result
+    return [{f'Puesto {i + 1}: {row['title']}'} for i, row in filter.iterrows()]
 
 
 @app.get("/UsersNotRecommend/{year}")
 async def UsersNotRecommend(year: int):
     """Función que devuelve los 3 juegos menos recomendados para un año dado."""
     # Se filtra las filas del DataFrame donde la columna 'release_year' es igual a year, la columna 'recommend' es False y la columna 'sentiment_analysis' con valor 0
-    filter = union_ur_sg['title'][(union_ur_sg['release_year'] == year) & (union_ur_sg['recommend'] is False) & (union_ur_sg['sentiment_analysis']==0)].value_counts().reset_index().head(3)
+    filter = union_ur_sg['title'][(union_ur_sg['release_year'] == year) & (union_ur_sg['recommend'] == False) & (union_ur_sg['sentiment_analysis']==0)].value_counts().reset_index().head(3)
 
-    result = [{'Puesto {}: {}'.format(i + 1, row['title'])} for i, row in filter.iterrows()]
-    
-    return result
+    return [{f'Puesto {i + 1}: {row['title']}'} for i, row in filter.iterrows()]
 
 
 @app.get("/sentiment_analysis/{year}")
